d7427d71166c335afd3d7ae35933379c36d9dc61
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-13 15:16:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,13 +74,15 @@ if submit_button:
         
         col1, col2 = st.columns(2)
         with col1:
-            st.subheader('Transcription text')
-            with open('transcription.txt', 'r') as f:
-                [st.write(line) for line in f.readlines()]
+            with st.expander('See Transcription text'):
+                st.subheader('Transcription text')
+                with open('transcription.txt', 'r') as f:
+                    [st.write(line) for line in f.readlines()]
         with col2:
-            st.subheader('Subtitle')
-            with open('transcription.srt', 'r') as f:
-                [st.write(line) for line in f.readlines()]
+            with st.expander('See Subtitle'):
+                st.subheader('Subtitle')
+                with open('transcription.srt', 'r') as f:
+                    [st.write(line) for line in f.readlines()]
         
         with open("transcription.zip", "rb") as zip_download:
             st.download_button(
